126a0b3fac3301db263fe15163a7bb82f4c99bf3
Jhojan Tobón <jhojantobonm@gmail.com>
2023-12-07 16:11:44 -0500

    Added useState.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,28 @@
 import { RandomFox } from "@/components/RandomFox";
 import { Inter } from "next/font/google";
+import { useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 const urlApi = "https://randomfox.ca/images";
 const randomNumber = () => Math.floor(Math.random() * 123) + 1;
 
 export default function Home() {
+  // can be string[]
+  const [images, setImages] = useState<Array<string>>([
+    `${urlApi}/${randomNumber()}.jpg`,
+    `${urlApi}/${randomNumber()}.jpg`,
+    `${urlApi}/${randomNumber()}.jpg`,
+    `${urlApi}/${randomNumber()}.jpg`,
+  ]);
+
   return (
     <main>
       <h1 className="text-center underline font-bold text-3xl">Hello world</h1>
-      <RandomFox api={`${urlApi}/${randomNumber()}.jpg`} />
+      {images.map((img, index) => (
+        <div key={index} className="p-4">
+          <RandomFox api={img} />
+        </div>
+      ))}
     </main>
   );
 }
