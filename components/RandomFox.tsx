49c2321bc9697d1ee2a305245641dc70fc35451a
Jhojan Tobón <jhojantobonm@gmail.com>
2023-12-07 15:33:21 -0500

    Destructured props.

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,5 +1,3 @@
-export const RandomFox = (props: { api: string }): JSX.Element => {
-  return (
-    <img className="rounded-lg" width={320} height={"auto"} src={props.api} />
-  );
+export const RandomFox = ({ api }: { api: string }): JSX.Element => {
+  return <img className="rounded-lg" width={320} height={"auto"} src={api} />;
 };
