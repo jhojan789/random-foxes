1ff317e929b0f02e641f51d18a59c0219d9c4c33
Jhojan Tobón <jhojantobonm@gmail.com>
2023-12-08 14:03:31 -0500

    Added custom types to the state

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,13 +6,29 @@ const inter = Inter({ subsets: ["latin"] });
 const urlApi = "https://randomfox.ca/images";
 const randomNumber = () => Math.floor(Math.random() * 123) + 1;
 
+type ImageType = {
+  id: string;
+  url: string;
+};
+function generateUniqueId() {
+  // Use the current timestamp as the basis for the unique ID
+  const timestamp = new Date().getTime();
+
+  // Generate a random number (in this case, a 4-digit number)
+  const random = Math.floor(Math.random() * 10000);
+
+  // Combine timestamp and random number to create the unique ID
+  const uniqueId = `${timestamp}${random}`;
+
+  return uniqueId;
+}
 export default function Home() {
   // can be string[]
-  const [images, setImages] = useState<Array<string>>([
-    `${urlApi}/${randomNumber()}.jpg`,
-    `${urlApi}/${randomNumber()}.jpg`,
-    `${urlApi}/${randomNumber()}.jpg`,
-    `${urlApi}/${randomNumber()}.jpg`,
+  const [images, setImages] = useState<Array<ImageType>>([
+    { id: generateUniqueId(), url: `${urlApi}/${randomNumber()}.jpg` },
+    { id: generateUniqueId(), url: `${urlApi}/${randomNumber()}.jpg` },
+    { id: generateUniqueId(), url: `${urlApi}/${randomNumber()}.jpg` },
+    { id: generateUniqueId(), url: `${urlApi}/${randomNumber()}.jpg` },
   ]);
 
   return (
@@ -20,7 +36,7 @@ export default function Home() {
       <h1 className="text-center underline font-bold text-3xl">Hello world</h1>
       {images.map((img, index) => (
         <div key={index} className="p-4">
-          <RandomFox api={img} />
+          <RandomFox api={img.url} />
         </div>
       ))}
     </main>
