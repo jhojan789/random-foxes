bb65fb00968244b5ebc7241dc96ef5a4a9c0b498
Jhojan Tobón <jhojantobonm@gmail.com>
2023-12-09 19:04:22 -0500

    Added observer and SVG Base64-encoded string.

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,4 +1,4 @@
-import { LegacyRef, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 type Props = {
   api: string;
@@ -6,6 +6,33 @@ type Props = {
 
 export const RandomFox = ({ api }: Props): JSX.Element => {
   const node = useRef<HTMLImageElement>(null);
+  const greyRectangleSVG = `
+  <svg xmlns="http://www.w3.org/2000/svg" width="320" height="320">
+    <rect width="100%" height="100%" fill="#808080">
+      <animate attributeName="opacity" values="0.3;1;0.3" dur="3s" repeatCount="indefinite" />
+    </rect>
+  </svg>
+`;
+
+  const base64Encoded = btoa(greyRectangleSVG);
+
+  const [src, setSrc] = useState(`data:image/svg+xml;base64,${base64Encoded}`);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setSrc(api);
+        }
+      });
+    });
+
+    if (node.current) {
+      observer.observe(node.current);
+    }
+
+    return () => observer.disconnect();
+  }, [api]);
 
   return (
     <img
@@ -13,7 +40,7 @@ export const RandomFox = ({ api }: Props): JSX.Element => {
       className="rounded-lg"
       width={320}
       height={"auto"}
-      src={api}
+      src={src}
     />
   );
 };
