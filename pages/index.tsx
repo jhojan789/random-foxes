ccb8728bb39ed9c71c02a5ea873b35bb9529e02b
Jhojan Tobón <jhojantobonm@gmail.com>
2023-12-11 15:19:40 -0500

    Imported as MouseEventHandler as type

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,6 @@
 import { LazyImage } from "@/components/RandomFox";
-import { log } from "console";
-import { Inter } from "next/font/google";
-import { MouseEventHandler, useState } from "react";
+import { useState, type MouseEventHandler } from "react";
 
-const inter = Inter({ subsets: ["latin"] });
 const urlApi = "https://randomfox.ca/images";
 const randomNumber = () => Math.floor(Math.random() * 123) + 1;
 
